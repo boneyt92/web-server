404492daa256ad23db7893aad28285cbd4246dc2
Bony Thomas <bothomas@tycoint.com>
2017-09-13 17:56:15 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const hbs = require('hbs');
+
+ var app = express();
+
+ // Express Middleware
+ app.use(express.static(__dirname + '/public'));
+ app.set('view engine','hbs');
+
+ app.get('/', (req,res)=> {
+    res.send("Hello Express!!!!");
+ });
+
+
+ app.get('/about',(req,res)=> {
+     res.send('About Page');
+ });
+
+ app.get('/bad',(req,res)=> {
+     res.send({
+         errormessage: 'Unable to handle the request'
+     });
+ });
+
+ app.listen(8000 , () => {
+     console.log('Server is up on port 8000');
+ }); 
+  
